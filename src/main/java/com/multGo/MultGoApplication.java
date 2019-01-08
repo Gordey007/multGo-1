c5692e362164b9639feac8477c71966f58f3e5a2
alex <astrgan@gmail.com>
2019-01-08 18:15:43 +1000

    Initial commit

---
+++ src/main/java/com/multGo/MultGoApplication.java
@@ -0,0 +1,14 @@
+package com.multGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultGoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/multGo/controllers/RickandmortyController.java
@@ -0,0 +1,16 @@
+package com.multGo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RickandmortyController {
+
+    @GetMapping("/rickandmorty")
+    public String rickandmorty(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "rickandmorty";
+    }
+}
