ec0f3295272584669156122b8dfdb46b9978c3a0
alex <astrgan@gmail.com>
2019-02-12 23:17:37 +1000

    foreach

--- src/main/java/com/multGo/controllers/RickandmortyController.java
+++ src/main/java/com/multGo/controllers/RickandmortyController.java
@@ -1,18 +1,17 @@
 package com.multGo.controllers;
 
 import com.multGo.entity.Cartoon;
-import com.multGo.logic.Parser;
 import com.multGo.repositorys.CartoonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 @Controller
 public class RickandmortyController {
@@ -21,12 +20,38 @@ public class RickandmortyController {
     CartoonRepository cartoonRepository;
     private Collection<Cartoon> cartoonArrayList;
 
-    @GetMapping("/cartoons/rickandmorty")
-    public String rickandmorty( Model model) {
+
+   /* @GetMapping(path = "rickandmorty/{season}")
+    public String rickandmorty(Model model, @PathVariable String season) {
+
+
+        cartoonArrayList = cartoonRepository.getCartoonBySeason(Integer.parseInt(season));
+        model.addAttribute("cartoonArrayList", cartoonArrayList);
+        model.addAttribute("seasons", cartoonRepository.getSeasons("Rick and Morty"));
+        System.out.println(season);
+        return "rickandmorty";
+    }*/
+
+    @GetMapping(path = "rickandmorty")
+    public String rickandmorty(Model model, @RequestParam(name = "season", required = false, defaultValue = "1") String season) {
+
+
+        cartoonArrayList = cartoonRepository.getCartoonBySeason(Integer.parseInt(season));
+        model.addAttribute("cartoonArrayList", cartoonArrayList);
+        model.addAttribute("seasons", cartoonRepository.getSeasons("Rick and Morty"));
+        model.addAttribute("season", season);
+        System.out.println(season);
+        return "rickandmorty";
+    }
+
+
+
+    @GetMapping("/cartoons/player")
+    public String player( Model model) {
 
         cartoonArrayList = (Collection<Cartoon>) cartoonRepository.findAll();
         model.addAttribute("cartoonArrayList", cartoonArrayList);
-        return "rickandmorty";
+        return "player";
     }
 
     @GetMapping("/parser")
--- src/main/java/com/multGo/repositorys/CartoonRepository.java
+++ src/main/java/com/multGo/repositorys/CartoonRepository.java
@@ -1,7 +1,19 @@
 package com.multGo.repositorys;
 
 import com.multGo.entity.Cartoon;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface CartoonRepository extends CrudRepository<Cartoon, Integer> {
+
+    Collection<Cartoon> getCartoonBySeason(Integer season);
+
+    @Query(value = "select max(season) from cartoons where name_cartoon = :name", nativeQuery = true)
+    int getSeasons(@Param("name") String name);
 }
